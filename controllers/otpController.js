6cb1d6f02e5e84dc54d310650d58d78412fe6ff0
p38325699-debug <p38325699@gmail.com>
2025-09-30 13:41:25 +0530

    Fix email config

--- controllers/otpController.js
+++ controllers/otpController.js
@@ -63,13 +63,23 @@ exports.sendOtp = async (req, res) => {
     }
 
     // Send OTP via email
+    // const transporter = nodemailer.createTransport({
+    //   service: "gmail",
+    //   auth: {
+    //     user: process.env.EMAIL_USER,
+    //     pass: process.env.EMAIL_PASS
+    //   }
+    // });
+
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // SSL
+  auth: {
+    user: process.env.EMAIL_USER, // Gmail address
+    pass: process.env.EMAIL_PASS, // App password
+  },
+});
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
